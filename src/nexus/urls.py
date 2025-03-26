d7750b7452f4896683fd9abb242872aac619272a
Murodali <kroost305@gmail.com>
2025-03-26 15:53:28 +0500

    O'zgarish haqida qisqacha izoh

--- src/nexus/urls.py
+++ src/nexus/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('product/', include('product.urls')),
-    # path('category/', include('category.urls'))
+    # path('category/', include('category.urls')),
+    path('user/', include('user.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
