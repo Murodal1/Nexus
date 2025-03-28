23d6a345585ae1934fd8e5bc39978569ab51cb50
Murodali <kroost305@gmail.com>
2025-03-28 15:33:09 +0500

    Register bo'ldi

--- src/user/models.py
+++ src/user/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50, null=False, blank=False)
+    firstname = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=True, blank=True)
-    phone_number = models.CharField(max_length=13, null=False, blank=False)
+    phone_number = models.CharField(max_length=13, null=True, blank=True)
     image = models.ImageField(upload_to='', null=True, blank=True)
 
     def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
+        return "%s %s" % (self.firstname, self.last_name)
 
 
